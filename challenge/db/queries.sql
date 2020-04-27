b82fa949a549265058641ca4ca19d78bba2f95bd
pbryzek <bryzek@gmail.com>
2020-04-26 20:44:35 -0700

    update query to pull from correct table

--- challenge/db/queries.sql
+++ challenge/db/queries.sql
@@ -43,23 +43,24 @@ ORDER BY emp_no;
 --Create emp_eligibility table
 --------
 SELECT 
-	emp.emp_no,
-	emp.first_name,
-	emp.last_name,
+	emp_title.emp_no,
+	emp_title.first_name,
+	emp_title.last_name,
 	emp.birth_date,
-	ti.title,
-	ti.from_date,
-	ti.to_date
+	emp_title.title,
+	emp_title.from_date,
+	de.to_date
 INTO emp_eligibility
-FROM employees AS emp
-INNER JOIN titles ti ON emp.emp_no = ti.emp_no
-INNER JOIN dept_emp de ON emp.emp_no = de.emp_no
+FROM emp_titles AS emp_title
+INNER JOIN dept_emp de ON emp_title.emp_no = de.emp_no
+INNER JOIN employees emp ON emp.emp_no = emp_title.emp_no
 WHERE de.to_date = '9999-01-01' AND (emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
 --------
 -- Queries to select from to test newly created tables
 --------
-SELECT * from emp_eligibility LIMIT 20;
-SELECT * from emp_titles LIMIT 20;
+SELECT * from emp_history LIMIT 10;
+SELECT * from emp_titles LIMIT 10;
+SELECT * from emp_eligibility LIMIT 10;
 
 -- Helper Query to find the emp_nos that have at least 2 titles.
 SELECT
